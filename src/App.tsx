ca2e0f4fd5acfa7f9b5e0cd3c18f62fa1047cead
Timur <astominsflavadept@gmail.com>
2024-05-30 18:12:51 +0500

    проект защищён, светлая тема больше не нужна

--- src/App.tsx
+++ src/App.tsx
@@ -22,10 +22,6 @@ export interface IData {
 	data: IDataElement[];
 }
 
-function changeTheme(): void {
-	document.body.classList.toggle("light_theme");
-}
-
 function App() {
 	const [activeList, setActiveList] = React.useState<boolean>(false);
 	const [data, setData] = React.useState<IDataElement[]>(dataJson);
@@ -51,36 +47,10 @@ function App() {
 	];
 
 	return (
-		<div className="app" onClick={() => setActiveList(false)}>
-			<button className="theme_btn" onClick={() => changeTheme()}>
-				<svg
-					width="48px"
-					height="48px"
-					viewBox="0 0 24 24"
-					xmlns="http://www.w3.org/2000/svg"
-					aria-labelledby="nightModeIconTitle"
-					stroke="#eee"
-					strokeWidth="1"
-					strokeLinecap="square"
-					strokeLinejoin="miter"
-					fill="none"
-					color="#eee"
-				>
-					{" "}
-					<path d="M12 19a7 7 0 1 0 0-14 7 7 0 0 0 0 14z"/>
-					{" "}
-					<path d="M15.899 12.899a4 4 0 0 1-4.797-4.797A4.002 4.002 0 0 0 12 16c1.9 0 3.49-1.325 3.899-3.101z"/>
-					{" "}
-					<path d="M12 5V3M12 21v-2"/>
-					{" "}
-					<path
-						d="M5 12H2h3zM22 12h-3 3zM16.95 7.05L19.07 4.93 16.95 7.05zM4.929 19.071L7.05 16.95 4.93 19.07zM16.95 16.95l2.121 2.121-2.121-2.121zM4.929 4.929L7.05 7.05 4.93 4.93z"/>
-					{" "}
-				</svg>
-			</button>
+		<div className='app' onClick={() => setActiveList(false)}>
 			{dataJson.length ? (
 				<>
-					<h1 className="app__heading">Статистика</h1>
+					<h1 className='app__heading'>Статистика</h1>
 
 					<SpecList
 						arrDate={arrDate}
@@ -89,18 +59,18 @@ function App() {
 						setActiveList={setActiveList}
 					/>
 
-					<h2 className="sec__heading">Средний балл</h2>
-					<MiddleGrade data={data}/>
-					<h2 className="sec__heading">Посещаемость</h2>
-					<Visits data={data}/>
-					<div className="actuality">
+					<h2 className='sec__heading'>Средний балл</h2>
+					<MiddleGrade data={data} />
+					<h2 className='sec__heading'>Посещаемость</h2>
+					<Visits data={data} />
+					<div className='actuality'>
 						Последняя пара была {day} {months[month]} {year} г.
 					</div>
 				</>
 			) : (
-				<div className="text_block">
+				<div className='text_block'>
 					Чтобы приложение заработало, нужно сделать следующее:
-					<ol className="instruction">
+					<ol className='instruction'>
 						<li>Зайти в журнал и нажать F12</li>
 						<li>Найти раздел "Сеть" и зайти в оценки</li>
 						<li>
@@ -116,7 +86,7 @@ function App() {
 							блокноте, то есть устанавливать среду разработки не нужно.
 						</li>
 					</ol>
-					<a href="/video.mp4" className="open_video" target="_blank">
+					<a href='/video.mp4' className='open_video' target='_blank'>
 						Открыть видеоинструкцию
 					</a>
 				</div>
