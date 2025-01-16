156c6dd9982984146b070d06a1f537c2c4635472
Timur <astominsflavadept@gmail.com>
2025-01-16 19:17:57 +0500

    в зачётке скрыты оценки без даты

--- src/components/Zachetka/Zachetka.tsx
+++ src/components/Zachetka/Zachetka.tsx
@@ -5,15 +5,20 @@ import { toFive } from "../../utils/toFive";
 export default function Zachetka({ data }: IZachetka) {
   return (
     <div className="zachetka">
-      {data.map((element) => (
-        <div className="zachetka__element">
-          <div className="zachetka__name">{element.spec}</div>
-          <div className="zachetka__grade">
-            {element.date <= "2024-09-01" ? toFive(element.mark) : element.mark}
-          </div>
-          <div className="zachetka__date">{element.date}</div>
-        </div>
-      ))}
+      {data.map(
+        (element) =>
+          element.date && (
+            <div className="zachetka__element">
+              <div className="zachetka__name">{element.spec}</div>
+              <div className="zachetka__grade">
+                {element.date <= "2024-09-01"
+                  ? toFive(element.mark)
+                  : element.mark}
+              </div>
+              <div className="zachetka__date">{element.date}</div>
+            </div>
+          )
+      )}
     </div>
   );
 }
