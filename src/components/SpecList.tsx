61aab059b9b1c05dd182dea5e06a8a042d9f7e1f
Timur <astominsflavadept@gmail.com>
2025-01-20 17:51:58 +0500

    Введена функция учитывания статистики за всё время или за текущий курс

--- src/components/SpecList.tsx
+++ src/components/SpecList.tsx
@@ -16,6 +16,7 @@ export default function SpecList({
   setActiveList,
 }: IProps) {
   const [activeSpec, setActiveSpec] = React.useState<string>("Все предметы");
+  const [considerPast, setConsiderPast] = React.useState<boolean>(false);
   const arr: string[] = dataJson
     .filter((_, pos) => new Date(dataJson[pos].date_visit) > new Date(arrDate))
     .map((i) => i.spec_name);
@@ -35,7 +36,7 @@ export default function SpecList({
   });
 
   return (
-    <>
+    <div className="flex">
       <div
         className="active_spec"
         onClick={(event) => {
@@ -44,41 +45,61 @@ export default function SpecList({
         }}
       >
         {activeSpec}
-      </div>
-      {activeList && (
-        <ul className="spec_list">
-          <li
-            className="spec_list__item"
-            onClick={() => {
-              setData(dataJson);
-              setActiveSpec("Все предметы");
-              setActiveList(false);
-            }}
-          >
-            Все предметы
-          </li>
-          {specList.map((spec) => (
+        {activeList && (
+          <ul className="spec_list">
             <li
               className="spec_list__item"
-              key={spec}
               onClick={() => {
-                setData(
-                  dataJson.filter(
-                    (element) =>
-                      element.spec_name === spec ||
-                      element.spec_name === `${spec} РПО` ||
-                      element.spec_name === `${spec} ГД`
-                  )
-                );
-                setActiveSpec(spec);
+                setData(dataJson);
+                setActiveSpec("Все предметы");
                 setActiveList(false);
               }}
             >
-              {spec}
+              Все предметы
             </li>
-          ))}
-        </ul>
-      )}
-    </>
+            {specList.map((spec) => (
+              <li
+                className="spec_list__item"
+                key={spec}
+                onClick={() => {
+                  if (considerPast) {
+                    setData(
+                      dataJson.filter(
+                        (element) =>
+                          element.spec_name === spec ||
+                          element.spec_name === `${spec} РПО` ||
+                          element.spec_name === `${spec} ГД`,
+                      ),
+                    );
+                  } else {
+                    setData(
+                      dataJson.filter(
+                        (element) =>
+                          (element.spec_name === spec ||
+                            element.spec_name === `${spec} РПО` ||
+                            element.spec_name === `${spec} ГД`) &&
+                          new Date(element.date_visit) > new Date(arrDate),
+                      ),
+                    );
+                  }
+
+                  setActiveSpec(spec);
+                  setActiveList(false);
+                }}
+              >
+                {spec}
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+
+      <button
+        className="toggle_consider"
+        onClick={() => setConsiderPast(!considerPast)}
+      >
+        {considerPast ? "За всё время" : "За текущий курс"}
+      </button>
+    </div>
   );
 }
